00da6a012e786198c3bc5d03f0bd844b62b6c07f
nostromo76 <ivans1976@gmail.com>
2023-07-01 12:51:10 +0200

    third comit tests

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Daily-Water-Calculator
+# Daily Water Calculator
+
 The Daily Water Calculator is a web application that helps users track their daily water intake and make sure they are drinking enough water every day. The application is built using HTML and JavaScript and is tested using Selenium.
+
+## Prerequisites
+
+Before you can run the application and the tests, please make sure you have the following installed:
+
+- Node.js
+- npm (Node Package Manager)
+
+## Installation
+
+To install the dependencies for this project, run the following command:
+
+```
+npm install
+```
+
+This will install the necessary packages such as Selenium, Selenium Webdriver, Mocha, and Mochawesome.
+
+## Usage
+
+To run the application, open the `Water_Consumption_ Calculator.html` file in your web browser.
+
+To run the tests, use the following command:
+
+```
+npm test
+```
+
+This runs the Mocha tests with a 10-second timeout and generates a Mochawesome report. The `--reporter mochawesome` option in the `package.json` file enables this report.
+
+To change the browser used for testing, update the `USE_BROWSER` variable in the `.env` file to your desired browser name (e.g. 'chrome', 'firefox', or 'safari'). By default, the application uses Chrome.
+USE_BROWSER = 'YOUR BROWSER'
+ driver = new Builder().forBrowser(process.env.USE_BROWSER).build()
+
+## Contributing
+
+If you find any issues or have suggestions for improvements, please submit them as issues on this project's Github repository. We welcome contributions to this project!
--- test/water_calculator.test.js
+++ test/water_calculator.test.js
@@ -1,4 +1,4 @@
-const { Builder} = require("selenium-webdriver");
+const { Builder } = require("selenium-webdriver");
 const { By } = require('selenium-webdriver');
 const CalculatorPage = require('../pages/BasePage')
 const utils = require('../data/utils');
@@ -17,12 +17,12 @@ describe('Test Water Calculator', async function () {
     beforeEach('Open Water Calculator', async function () {
         driver = new Builder().forBrowser(process.env.USE_BROWSER).build()
         Calculator = new CalculatorPage(driver)
-        
+
     })
     afterEach('Close browser', function () {
         Calculator.closeBrowser()
     })
-    it('Test stranice ', async function () {
+    it('Test page ', async function () {
         await Calculator.visit(fileUrl)
         await driver.manage().window().maximize();
         const title = await Calculator.getTitle();
@@ -30,25 +30,70 @@ describe('Test Water Calculator', async function () {
         await driver.sleep(3000);
     });
 
-    it('Testiranje naslova h1', async function () {
-       
+    it('Test of  h1', async function () {
+
         await Calculator.visit(fileUrl);
-        await driver.manage().window().maximize();
+        // await driver.manage().window().maximize();
         const title = await driver.findElement(By.css('h1'));
         const text = await title.getText();
         assert.ok(text.includes('Water Consumption Calculator'));
     });
-    it('Test Water Calculator', async function() {
-       
+    it('Test Inputs fields', async function () {
+        await Calculator.visit(fileUrl);
+        await driver.executeScript('window.scrollBy(0, 500);');
+
+        let weightInput = await driver.findElement(By.id('weight'));
+        let exerciseInput = await driver.findElement(By.id('exercise'));
+        await weightInput.sendKeys('80');
+        await exerciseInput.sendKeys('30');
+        let calculateButton = await driver.findElement(By.id('calculate'));
+        await calculateButton.click();
+
+        let requiredWaterOutput = await driver.findElement(By.id('required_water'));
+        let requiredWaterValue = await requiredWaterOutput.getAttribute('value');
+        assert.equal(requiredWaterValue, '52', 'Required daily amount in ounces:');
+
+        let waterBottlesOutput = await driver.findElement(By.id('water_bottles'));
+        let waterBottlesValue = await waterBottlesOutput.getAttribute('value');
+        assert.equal(waterBottlesValue, '3.08', 'Number of 16.9 ounce water bottles needed:');
+
+
+        /* Better way
+        await driver.executeScript("window.scrollBy(0,document.body.scrollHeight)")
         const weightInput = await driver.findElement(By.id('weight'));
         const exerciseInput = await driver.findElement(By.id('exercise'));
         await weightInput.sendKeys('80');
         await exerciseInput.sendKeys('30');
         const calculateButton = await driver.findElement(By.id('calculate'));
         await calculateButton.click();
-        
-     });
- 
-})
+        const reqWater = await driver.findElement(By.id('required_water'))
+        const botles = await driver.findElement(By.id('water_bottles'))
+        assert.equal(await reqWater.getAttribute('value'), '52')
+        assert.equal(await botles.getAttribute('value'), '3.08')
+       */
+
+        let clearButton = await driver.findElement(By.id('clear'));
+        await clearButton.click();
+
+        requiredWaterValue = await requiredWaterOutput.getAttribute('value');
+        assert.equal(requiredWaterValue, '');
+
+        waterBottlesValue = await waterBottlesOutput.getAttribute('value');
+        assert.equal(waterBottlesValue, '');
+    });
+    it('Test Clear Button', async function () {
+        await Calculator.visit(fileUrl);
+        await driver.executeScript('window.scrollBy(0, 500);');
+        let clearButton = await driver.findElement(By.id('clear'));
+        await clearButton.click();
+
+        const reqWater = await driver.findElement(By.id('required_water'))
+        const botles = await driver.findElement(By.id('water_bottles'))
+        assert.equal(await reqWater.getAttribute('value'), '')
+        assert.equal(await botles.getAttribute('value'), '')
+    });
+
+});
+
 
 
